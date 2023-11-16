65524a22914863e75a428b49bdf41aeb3ad3d74c
WhiteWalker44 <sathavarahardik00h@gmail.com>
2023-11-16 09:08:12 +0530

    Filter Added

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.CryptoView.as_view(), name="crypto_view")
+    path("", views.CryptoView.as_view(), name="crypto_view"),
+    path("<str:filter_by>", views.CryptoView.as_view(), name="filter_view")
 ]
--- app/views.py
+++ app/views.py
@@ -2,60 +2,97 @@ from django.shortcuts import render, HttpResponse
 from django.views import View
 import requests
 import json
-
+from .forms import RankingFilterForm
 # Create your views here.
 
 
+# class CryptoView(View):
+#     def get(self, request, page_number=None, filter_by=None):
+#         # from Tradingbeez.settings import ALPHA_VOLTAGE_API_KEY
+#         # key = ALPHA_VOLTAGE_API_KEY
+#         # # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+#         # url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={key}'
+#         # r = requests.get(url)
+#         # data = r.json()
+
+#         # # print(json.dumps(data))
+
+#         # data = json.dumps(data)
+#         # context = {}
+#         # context['data'] = data
+#         # # print(context)
+
+
+#         form = RankingFilterForm()
+#         api_url = "https://api.coincap.io/v2"
+#         endpoint = "assets"
+#         params = {}
+#         page_number = 1
+
+
+
+#         if filter_by is not None:
+#             limit_per_page = 10
+#             if filter_by == "top":
+#                 params = {
+#                     "limit": limit_per_page,
+#                     "offset": (page_number - 1) * limit_per_page,
+#                 }
+#             elif filter_by == "worst":
+#                 params = {
+#                     "limit": limit_per_page,
+#                     "offset": (11 - 1) * limit_per_page,
+#                 }
+            
+
+#         response = requests.get(f"{api_url}/{endpoint}", params=params)
+#         data = response.json()
+
+#         print(data)
+
+#         context = {}
+#         context["data"] = data
+#         context["page_number"] = page_number
+#         context["form"]= form
+#         return render(request, "crypto.html", context=context)
+
+
+
+
 class CryptoView(View):
-    def get(self, request, page_number=None):
-        
-        
-        
-        # from Tradingbeez.settings import ALPHA_VOLTAGE_API_KEY
-        # key = ALPHA_VOLTAGE_API_KEY
-        # # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-        # url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={key}'
-        # r = requests.get(url)
-        # data = r.json()
-
-        # # print(json.dumps(data))
-
-        # data = json.dumps(data)
-        # context = {}
-        # context['data'] = data
-        # # print(context)
+    def get(self, request, filter_by=None):
+        form = RankingFilterForm(request.GET)
 
         api_url = "https://api.coincap.io/v2"
         endpoint = "assets"
-        limit_per_page = 10
+        params = {}
+        page_number = 1
 
-        page_number = page_number
-        print(page_number, type(page_number))
-        if page_number is None:
-            page_number = 1
-
-        else:
-            page_number = page_number
-
-        params = {
-            "limit": limit_per_page,
-            "offset": (page_number - 1) * limit_per_page,
-        }
+        print(form,"===========>>>>>>>>>")
+        
+        if filter_by is not None:
+            limit_per_page = 10
+            if filter_by == "top":
+                params = {
+                    "limit": limit_per_page,
+                    "offset": (page_number - 1) * limit_per_page,
+                }
+            elif filter_by == "worst":
+                params = {
+                    "limit": limit_per_page,
+                    "offset": (100 - limit_per_page),
+                }
 
         response = requests.get(f"{api_url}/{endpoint}", params=params)
         data = response.json()
 
-        print(data)
-
-
-        context = {}
-        context["data"] = data
-        context["page_number"] = page_number
+        context = {
+            "data": data,
+            "page_number": page_number,
+            "form": form,
+        }
         return render(request, "crypto.html", context=context)
-
 
 class PaginnationView(View):
     def get(self, request):
         data = request.get("data")
-
-
